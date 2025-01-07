45450b19b63954e3c1f2ab01d6ca1c9293f255cd
Gabriel <gabrielmassaia50@gmail.com>
2025-01-06 21:52:08 -0300

    criação da interface Artista

--- ScreenSound/Database/Connection.cs
+++ ScreenSound/Database/Connection.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Data.SqlClient;
+using ScreenSound.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +11,34 @@ namespace ScreenSound.Database
 {
     internal class Connection
     {
-        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;" +
+            "Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
         public SqlConnection ObterConexao()
         {
             return new SqlConnection(connectionString);
         }
-    }
 
-    
+        public IEnumerable<Artista> Listar()
+        {
+            var lista = new List<Artista>();
+            using var connection = ObterConexao();
+            connection.Open();
+
+            String sql = "SELECT * FROM Artistas";
+            SqlCommand command = new SqlCommand(sql, connection);
+            using SqlDataReader dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                string nomeArtista = Convert.ToString(dataReader["Nome"]);
+                string bioArtista = Convert.ToString(dataReader["bio"]);
+                int idArtista = Convert.ToInt32(dataReader["Id"]);
+
+                Artista artista = new(nomeArtista, bioArtista) { Id = idArtista };
+                lista.Add(artista);
+            }
+            return lista;
+        }
+    }
 }
